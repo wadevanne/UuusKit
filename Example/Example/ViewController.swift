50d00e873df176aee20b70f0b5d7109f9ca663f8
wadevanne <wadevanne@icloud.com>
2017-12-09 10:30:40 +0800

    "${SRCROOT}/../Example/Example/" "${SRCROOT}/../Example/ExampleTests/" "${SRCROOT}/../Example/ExampleUITests/"

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,50 +9,48 @@
 import UuusKit
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         if #available(iOS 10.0, *) {
-            _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
-                
+            _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             }
         } else {
             // Fallback on earlier versions
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
-    
+
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    @IBAction func action(_ sender: UIButton) {
+
+    @IBAction func action(_: UIButton) {
     }
 }
